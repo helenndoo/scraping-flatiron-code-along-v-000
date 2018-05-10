1c928b7d05440210aac8a9b9d7a5b032acd8d9d4
Helen Du <helendu.614@gmail.com>
2018-05-10 17:40:44 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,9 @@ require_relative './course.rb'
 
 class Scraper
 
+  def get_page
+    doc = Nokogiri::HTML(open(""http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
